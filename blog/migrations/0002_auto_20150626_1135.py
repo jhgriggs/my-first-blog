4305402ccc453bda0dd5b9e1e01d851a76f25943
Janette Griggs <jhgriggs.github@gmail.com>
2015-06-26 12:09:06 -0700

    My Django Girls app, first commit

---
+++ blog/migrations/0002_auto_20150626_1135.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='created_date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 6, 26, 18, 35, 19, 323666, tzinfo=utc)),
+        ),
+    ]
